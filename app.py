ddac7a64c95c4744a7b60a6c2d7a62d70fb00ed3
vennreddy490 <vennreddy490@gmail.com>
2024-04-26 16:55:45 -0400

    venn commit

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+import pandas as pd
+import sqlite3
+from database import get_db
 app = Flask(__name__)
 
 @app.route("/")
 def home(): 
-    return "<h1> Hello World! </h1>"
+    (cursor, conn) = get_db()
+
+    cursor.execute("SELECT * FROM table_name")
+    rows = cursor.fetchall()
+    counter = 0
+    print(type(rows))
+    # for row in rows:
+    #     print(row)
+    print(rows[1])
+    for i in range(0, 5):
+        print(rows[i])
+
+
+    return "<h1> {{rows}} </h1>"
--- database.py
+++ database.py
@@ -1,22 +1,28 @@
 import sqlite3
-
-# Step 1: Read the CSV file into a DataFrame
 import pandas as pd
-df = pd.read_csv('athlete_events.csv')
 
-# Step 2: Connect to the SQLite database and create it if it doesn't exist
-conn = sqlite3.connect('your_database.db')
+def get_db():
+    # Step 1: Read the CSV file into a DataFrame
+    df = pd.read_csv('athlete_events.csv')
 
-# Step 3: Write DataFrame to the database
-df.to_sql('table_name', conn, if_exists='replace', index=False)
+    # Step 2: Connect to the SQLite database and create it if it doesn't exist
+    conn = sqlite3.connect('your_database.db')
 
-# Step 4: Perform queries
-cursor = conn.cursor()
-# cursor.execute("SELECT * FROM table_name WHERE condition")
-cursor.execute("SELECT * FROM table_name")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    # Step 3: Write DataFrame to the database
+    df.to_sql('table_name', conn, if_exists='replace', index=False)
 
-# Step 5: Close the connection
-conn.close()
+    # Step 4: Perform queries
+    cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM table_name WHERE condition")
+    cursor.execute("SELECT * FROM table_name")
+    rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row)
+
+    # Step 5: Close the connection
+    # conn.close()
+
+    return (cursor, conn)
+
+def close_db(conn):
+    conn.close()
