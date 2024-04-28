50f086e028207f6ac4d68cde4270fabd894e53f0
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-04-28 17:34:24 -0400

    passing table object to frontend

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import pandas as pd
 import sqlite3
 from database import get_db
+from markupsafe import Markup
 from flask import render_template
 app = Flask(__name__)
 
@@ -21,11 +22,49 @@ def home():
 
     data = rows[3]
     data2 = rows[4]
-    
-    return render_template('index.html', dataToRender=data, dataToRender2=1) 
+    my_list = [['Name', 'Age', 'Country'],
+           ['John', '25', 'USA'],
+           ['Emily', '30', 'Canada'],
+           ['David', '27', 'Australia']]
+
+    html_table = list_to_html_table(my_list)
+    # value = Markup(html_table)
+    return render_template('index.html', dataToRender=html_table, dataToRender2=1) 
 
 @app.route("/query1") 
 def query1(): 
     return 
+
+def list_to_html_table(data):
+    # Generate the table header
+    table_html = '<table>\n<thead>\n<tr>'
+    for header in data[0]:
+        table_html += f'<th>{header}</th>'
+    table_html += '</tr>\n</thead>\n'
+
+    # Generate the table rows
+    table_html += '<tbody>\n'
+    for row in data[1:]:
+        table_html += '<tr>'
+        for value in row:
+            table_html += f'<td>{value}</td>'
+        table_html += '</tr>\n'
+    table_html += '</tbody>\n'
+
+    # Close the table tag
+    table_html += '</table>'
+
+    return Markup(table_html)
+
+
+# # Example usage
+# my_list = [['Name', 'Age', 'Country'],
+#            ['John', '25', 'USA'],
+#            ['Emily', '30', 'Canada'],
+#            ['David', '27', 'Australia']]
+
+# html_table = list_to_html_table(my_list)
+# print(html_table)
+
 
 
