c07c8a55f01190638417b1795dc1ecc4eb085beb
vennreddy490 <vennreddy490@gmail.com>
2024-04-28 22:30:23 -0400

    added second query functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import pandas as pd
 import sqlite3
-from database import get_db, get_events_results_db 
+from database import get_db, get_events_results_db, get_athlete_information_db
 from markupsafe import Markup
 from flask import render_template
 app = Flask(__name__)
@@ -33,31 +33,31 @@ def home():
 
 @app.route("/query1") 
 def query1(): 
-    (cursor, conn) = get_events_results_db()
+    (cursor, conn) = get_athlete_information_db()
 
-    cursor.execute("SELECT * FROM events_results")
+    cursor.execute("SELECT * FROM athlete_information")
     rows = cursor.fetchall()
     counter = 0
-    # print(type(rows))
     for row in rows:
         print(row)
-        counter = counter + 1
+        counter += 1
         if (counter == 10):
             break
-    # print(rows[1])
-    # for i in range(0, 5):
-    #     print(rows[i])
 
-    data = rows[3]
-    data2 = rows[4]
-    my_list = [['Name', 'Age', 'Country'],
-           ['John', '25', 'USA'],
-           ['Emily', '30', 'Canada'],
-           ['David', '27', 'Australia']]
-
-    html_table = list_to_html_table(my_list)
+    html_table = list_to_html_table(rows)
     # value = Markup(html_table)
-    return render_template('index.html', dataToRender=html_table, dataToRender2=1) 
+    return render_template('query1.html', dataToRender=html_table, dataToRender2=1) 
+
+@app.route("/query2") 
+def query2(): 
+    (cursor, conn) = get_events_results_db()
+
+    cursor.execute("SELECT * FROM events_results LIMIT 100")
+    rows = cursor.fetchall()
+
+    html_table = list_to_html_table(rows)
+    # value = Markup(html_table)
+    return render_template('query2.html', dataToRender=html_table, dataToRender2=1) 
 
 def list_to_html_table(data):
     # Generate the table header
--- database.py
+++ database.py
@@ -50,3 +50,27 @@ def get_events_results_db():
     # conn.close()
 
     return (cursor, conn)
+
+def get_athlete_information_db():
+    # Step 1: Read the CSV file into a DataFrame
+    df = pd.read_csv('schemas/athlete_information.csv')
+
+    # Step 2: Connect to the SQLite database and create it if it doesn't exist
+    conn = sqlite3.connect('your_database.db')
+
+    # Step 3: Write DataFrame to the database
+    df.to_sql('athlete_information', conn, if_exists='replace', index=False)
+
+    # Step 4: Perform queries
+    cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM table_name WHERE condition")
+    cursor.execute("SELECT * FROM athlete_information")
+    rows = cursor.fetchall()
+
+    # for row in rows:
+    #     print(row)
+
+    # Step 5: Close the connection
+    # conn.close()
+
+    return (cursor, conn)
